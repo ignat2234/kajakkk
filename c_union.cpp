825ec5683f93cac65b3f58477fdc78ae6c8f6a60
kmail ignatowicz <ignatowicz.kamil@wp.pl>
2022-03-17 10:57:54 +0100

    CL

--- c_union.cpp
+++ c_union.cpp
@@ -1,4 +1,18 @@
-//
-// Created by ZST on 3/10/2022.
-//
+#include <iostream>
 
+using namespace std;
+union student{
+    char imie;
+    int ocena;
+};
+
+int main() {
+    student uczen;
+    uczen.ocena = 4;
+    uczen.imie = 3;
+    cout << sizeof(student) << endl;
+   // cout << uczen.ocena;
+    // wypisze 5 bo jest wpizana ostatnia z union
+
+    return 0;
+}
